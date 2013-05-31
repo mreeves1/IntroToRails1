9bfecdfa19478c9398c836b4b05caa321af91ffd
Michael Reeves <mreeves@kabam.com>
2013-05-31 14:57:37 -0700

    initial commit of rails tasks tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '7fb6a033e6164230569e16d4901c93312271bb548534358bd5db319f02d4b510e7c75bc74d76d80e9719ced49c053fa2fecdc9792717798a43acd2ba532775ef'
